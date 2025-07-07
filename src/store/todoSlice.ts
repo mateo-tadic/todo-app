db476aaf452eb2920589d07a9a52b9d942a6d022
Mateo Tadic <mateo.tadic@2s-info-media.de>
2025-07-07 10:08:33 +0200

    feat: add todos slice with add, toggle and delete reducers

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodosState {
+  todos: Todo[];
+}
+
+const initialState: TodosState = {
+  todos: [],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const newTodo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      const todoId = action.payload;
+      const todoToToggle = state.todos.find((todo) => todo.id === todoId);
+      if (todoToToggle) todoToToggle.completed = !todoToToggle.completed;
+    },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      const todoId = action.payload;
+      state.todos = state.todos.filter((todo) => todo.id !== todoId);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer; 
